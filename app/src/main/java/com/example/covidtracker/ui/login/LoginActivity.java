a3996a2058bd9e3f6b333dad528d7a21214b8a35
mtkatsaros <60672119+mtkatsaros@users.noreply.github.com>
2020-11-02 23:08:15 -0800

    Began Firebase Login setup

--- app/src/main/java/com/example/covidtracker/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/login/LoginActivity.java
@@ -31,6 +31,7 @@ import com.example.covidtracker.ui.login.LoginViewModel;
 import com.example.covidtracker.ui.login.LoginViewModelFactory;
 import com.example.covidtracker.ui.status.StatusActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,11 +40,13 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private FirebaseAuth mAuth;
-    public static final String TAG = "Login";
+    public static final String TAG = "Covid Tracker";
     public static final String COLLECTION_USERS = "users";
     private EditText usernameEditText;
     private EditText passwordEditText;
@@ -54,59 +57,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        /*loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
-        */
-
         usernameEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         loginButton.setEnabled(false);
         loadingProgressBar = findViewById(R.id.loading);
 
-
         //Instantiate the authenticator
         mAuth = FirebaseAuth.getInstance();
-
-        /*
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                startActivity(new Intent(LoginActivity.this, ExposureActivity.class));
-                finish();
-            }
-        });
-        */
-
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -172,29 +130,11 @@ public class LoginActivity extends AppCompatActivity {
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 
+                            //initialize the user to the database
                             FirebaseFirestore reference = FirebaseFirestore.getInstance();
-                            DocumentReference docRef = reference.collection(COLLECTION_USERS)
-                                    .document(email);
-                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if(task.isSuccessful()){
-                                        DocumentSnapshot document = task.getResult();
-
-                                        //check if the account is in the database
-                                        if(!document.exists()){
-                                            //Add email to database upon account creation
-
-                                            FirebaseFirestore reference = FirebaseFirestore.getInstance();
-                                            reference.collection(COLLECTION_USERS).add(email);
-
-                                        }
-
-
-                                    }
-                                }
-                            });
-
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("covid", "false");
+                            reference.collection(COLLECTION_USERS).document(email).set(map);
 
                             updateUI(user);
                         } else {
