a1443534fe30d94394e6ec45a1cc9ec72b8fe2ec
Alicia Stanton <atkelly013113@gmail.com>
2020-11-11 11:49:18 -0800

    Converting Maps Button to ImageButton

--- app/src/main/java/com/example/covidtracker/ui/status/StatusActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/status/StatusActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.covidtracker.R;
 import com.example.covidtracker.ui.exposure.ExposureActivity;
@@ -17,7 +18,10 @@ public class StatusActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
 
-        Button button = (Button) findViewById(R.id.button_sta);
+       /* Button button = (Button) findViewById(R.id.button_sta); */
+
+        ImageButton mapsButton = (ImageButton)findViewById(R.id.mapsButton);
+        mapsButton.setImageResource(R.drawable.mapIcon);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
