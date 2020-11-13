9890ecedb6c5f562073952912eabbb3031592a63
GE <ellergabriel13@gmail.com>
2020-11-13 05:05:47 -0800

    Got county lines displayed; not efficient, definitely needs another look

--- app/src/main/java/com/example/covidtracker/ui/exposure/ExposureActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/exposure/ExposureActivity.java
@@ -3,21 +3,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+
 import com.example.covidtracker.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
+import com.google.maps.android.data.geojson.GeoJsonPointStyle;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -25,12 +25,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
+
 public class ExposureActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    public static HashMap<String, String[]> counties;
+    public static HashMap<String, String[]> counties =  new HashMap<String, String[]>();
+    public static JSONObject obj;
 
-    public class CountyInformation extends AsyncTask<URL, Integer, Long> {
+    public class CountyInformation extends AsyncTask<URL, Integer, JSONObject> {
 
+        public JSONObject obj;
         //Indices for reading ActNow API
         private static final int COUNTY_FIPS = 0;
         private static final int STATE = 2;
@@ -48,27 +51,50 @@ public class ExposureActivity extends AppCompatActivity implements OnMapReadyCal
         }
 
         @Override
-        protected Long doInBackground(URL... urls) {
+        protected JSONObject doInBackground(URL... urls) {
             try {
-                URL actNow = new URL("https://api.covidactnow.org/v2/counties.csv?apiKey=5035fb4640c64817a4bbb23a6f135289");
+                URL actNow = new URL(urls.toString());
                 HttpURLConnection connect = (HttpURLConnection) actNow.openConnection();
 
+                /*
                 //File tester = new File(connect.getInputStream());
                 BufferedReader read = new BufferedReader(new InputStreamReader(connect.getInputStream()));
                 String line = "";
                 String[] split;
+                String[] store = new String[6];
                 while ((line = read.readLine()) != null) {
                     split = line.split(",");
-                    String[] countyInfo = {split[STATE], split[COUNTY_NAME], split[POPULATION_INDEX], split[CASE_INDEX], "", ""};//Empty Strings for lat/long coordinates
-                    counties.put(split[COUNTY_FIPS], countyInfo);
+                    //String[] countyInfo = {split[STATE], split[COUNTY_NAME], split[POPULATION_INDEX], split[CASE_INDEX], "", ""};//Empty Strings for lat/long coordinates
+                    store[0] = split[STATE];
+                    store[1] = split[COUNTY_NAME];
+                    store[2] = split[POPULATION_INDEX];
+                    store[3] = split[CASE_INDEX];
+                    store[4] = store[5] = "";
+                    counties.put(split[COUNTY_FIPS], store);
                 }
+                */
 
+                //System.out.println("shazbot" + counties.size());
+                //URL celeste = new URL("https://doc-00-7o-docs.googleusercontent.com/docs/securesc/k0u30gf6u7fqfnkjs2ca2mu9gqj4rkrf/knlrjs91mgtrkpvpmoguid52esm0t10p/1605252525000/16212445481544018714/16212445481544018714/1CVeXOgv4f53ikJoWUaZFGQyGpDgF1oHO?e=download&authuser=1&nonce=jh9jp9ktu63ha&user=16212445481544018714&hash=an6dl66mc6dua4nqeae97t253p0ibpc5");
+                //HttpURLConnection connectCounty = (HttpURLConnection) celeste.openConnection();
+                //KmlLayer layer = new KmlLayer(gMap, connectCounty.getInputStream(), this);
+
+                BufferedReader read = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+                String jsonText = "";
+                StringBuilder build = new StringBuilder();
+                while ((jsonText = read.readLine()) != null){
+                    build.append(jsonText);
+                }
+                jsonText = build.toString();
+                obj = new JSONObject(jsonText);
+                return obj;
             } catch (MalformedURLException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
+            }catch (JSONException e) {
             }
             return null;
         }
@@ -84,22 +110,25 @@ public class ExposureActivity extends AppCompatActivity implements OnMapReadyCal
         setContentView(R.layout.exposure);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-
-        //new FetchDataTask().execute(URL);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap){
-        gMap = googleMap;
-        //GeoJsonLayer layer;
-        LatLng riteAid = new LatLng(33.182, -117.369);
-        LatLng lincoln = new LatLng(33.1896, -117.349);
-        gMap.addMarker(new MarkerOptions().position(riteAid).title("Positive"));
-        gMap.addMarker(new MarkerOptions().position(lincoln).title("Positive"));
-        gMap.setMinZoomPreference(12);
-        gMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(33.18229, -117.3264)));
+    public void onMapReady(GoogleMap googleMap) {
 
+        gMap = googleMap;
+        try {
+            GeoJsonLayer layer = new GeoJsonLayer(gMap, R.raw.geojson, this);
+            layer.addLayerToMap();
+            GeoJsonPointStyle style = layer.getDefaultPointStyle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        gMap.setMinZoomPreference(4);
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(40, -99)));
 
         gMap.getUiSettings().setZoomControlsEnabled(true);
         gMap.getUiSettings().setZoomGesturesEnabled(true);
@@ -107,9 +136,17 @@ public class ExposureActivity extends AppCompatActivity implements OnMapReadyCal
         gMap.getUiSettings().setCompassEnabled(true);
         gMap.getUiSettings().setRotateGesturesEnabled(false);
 
-        
+    }
+
+    public void readFile(){
+        //JSONObject jObj = new JSONObject(t);
+
     }
 
 
     }
+
+
+
+
 
