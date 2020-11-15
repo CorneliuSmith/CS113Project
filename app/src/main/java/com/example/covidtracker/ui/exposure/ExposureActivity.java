9a427fd86197c927b71676940c764ba7363a09ef
Isaiah Smith <isaiah.cornelius.smith@gmail.com>
2020-11-15 08:28:52 -0800

    Add time information to exposure

--- app/src/main/java/com/example/covidtracker/ui/exposure/ExposureActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/exposure/ExposureActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 import com.example.covidtracker.R;
 import com.example.covidtracker.ui.status.StatusActivity;
@@ -11,10 +19,17 @@ import com.example.covidtracker.ui.status.StatusActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExposureActivity extends AppCompatActivity {
+    Date date = Calendar.getInstance().getTime();
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+    String strDate = dateFormat.format(date);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exposure);
+
+        TextView exposureText = (TextView) findViewById(R.id.textView2);
+        exposureText.setText(strDate);
 
         Button button = (Button) findViewById(R.id.button_exp);
 
