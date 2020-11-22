5efb218a2b0dd365c625ab6944b353fbf1a6e90f
Alicia Stanton <atkelly013113@gmail.com>
2020-11-22 10:55:05 -0800

    Adding Connection from LogOut Button (on Settings Page) to navigate back to LoginActivity

--- app/src/main/java/com/example/covidtracker/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/settings/SettingsActivity.java
@@ -6,14 +6,30 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.covidtracker.R;
+import com.example.covidtracker.ui.login.LoginActivity;
 import com.example.covidtracker.ui.map.MapActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        Button button = (Button) findViewById(R.id.logout_Button);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+            }
+
+        });
     }
 }
