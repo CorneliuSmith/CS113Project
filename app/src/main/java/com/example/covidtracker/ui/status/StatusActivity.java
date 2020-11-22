0764edb41b58d4450d121461acd6db8e3f5ad189
Alicia Stanton <atkelly013113@gmail.com>
2020-11-22 12:05:43 -0800

    Removing toggles and updating import statements

--- app/src/main/java/com/example/covidtracker/ui/status/StatusActivity.java
+++ app/src/main/java/com/example/covidtracker/ui/status/StatusActivity.java
@@ -10,7 +10,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity; */
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.support.*;
+
 
 
 import com.example.covidtracker.R;
